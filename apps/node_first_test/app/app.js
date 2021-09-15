0ad0e4d54b2241a6f57db9c93385352a834e2b86
root <jermwatt@gmail.com>
2021-09-15 16:09:50 +0000

    up

--- apps/node_first_test/app/app.js
+++ apps/node_first_test/app/app.js
@@ -2,17 +2,29 @@
 const http = require('http');
 const port = 80;
 const hostname = '0.0.0.0';
+const fs = require('fs')
 
 // define server
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  fs.readFile('index.html',function(error,data) 
+  {
+    if (error)
+    {
+      res.writeHead(404)
+      res.write('error: file not found')
+    }
+    else
+    {
+      res.write(data)
+    }
+    res.end();
+  })
 });
 
+
 // start listeing
-// server.listen(port, hostname, function(error)) => {
-server.listen(port, hostname, (error) => {
+server.listen(port, hostname, function(error) {
     if (error)
     {
         console.log('something went wrong',error)
